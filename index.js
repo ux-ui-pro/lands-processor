c495dc97894305e73b23bc8f3330d5db08389df5
ux-ui-pro <maxim@ux-ui.pro>
2024-07-17 13:54:17 +0300

    release 1.0.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const { JSDOM } = require('jsdom');
 
@@ -14,26 +14,27 @@ class LandsProcessor {
     this.#dom = null;
   }
 
-  process() {
-    this.loadHtml();
-    this.processLinks();
-    this.processScripts();
-    this.processImages();
-    this.processSrcSet();
-    this.processBackgroundImages();
-    this.processHead();
-    this.processBody();
-    this.organizeAssets();
-    this.saveHtml();
+  async process() {
+    await this.loadHtml();
+    await this.processLinks();
+    await this.processScripts();
+    await this.processImages();
+    await this.processSrcSet();
+    await this.processBackgroundImages();
+    await this.processHead();
+    await this.processGeo();
+    await this.organizeAssets();
+    await this.processCssFiles();
+    await this.saveHtml();
   }
 
-  loadHtml() {
-    const html = fs.readFileSync(this.#htmlFilePath, 'utf8');
+  async loadHtml() {
+    const html = await fs.readFile(this.#htmlFilePath, 'utf8');
 
     this.#dom = new JSDOM(html);
   }
 
-  processLinks() {
+  async processLinks() {
     const { document } = this.#dom.window;
 
     document.querySelectorAll('link[href]').forEach((link) => {
@@ -45,7 +46,7 @@ class LandsProcessor {
     });
   }
 
-  processScripts() {
+  async processScripts() {
     const { document } = this.#dom.window;
 
     document.querySelectorAll('script[src]').forEach((script) => {
@@ -55,7 +56,7 @@ class LandsProcessor {
     });
   }
 
-  processImages() {
+  async processImages() {
     const { document } = this.#dom.window;
 
     document.querySelectorAll('img[src]').forEach((img) => {
@@ -65,7 +66,7 @@ class LandsProcessor {
     });
   }
 
-  processSrcSet() {
+  async processSrcSet() {
     const { document } = this.#dom.window;
 
     document.querySelectorAll('[srcset]').forEach((element) => {
@@ -80,7 +81,7 @@ class LandsProcessor {
     });
   }
 
-  processBackgroundImages() {
+  async processBackgroundImages() {
     const { document } = this.#dom.window;
 
     document.querySelectorAll('[style*="background-image"]').forEach((element) => {
@@ -96,15 +97,15 @@ class LandsProcessor {
     });
   }
 
-  saveHtml() {
+  async saveHtml() {
     const updatedHtml = this.#dom.serialize();
 
-    fs.writeFileSync('dist/main.twig', updatedHtml);
+    await fs.writeFile('dist/main.twig', updatedHtml);
 
     console.log('The main.twig file has been successfully created.');
   }
 
-  processHead() {
+  async processHead() {
     const { document } = this.#dom.window;
     const title = document.querySelector('title');
 
@@ -121,50 +122,89 @@ class LandsProcessor {
     console.log('The functions have been successfully added to the head section.');
   }
 
-  processBody() {
+  async processGeo() {
     const { document } = this.#dom.window;
     const body = document.querySelector('body');
+    const html = document.querySelector('html');
 
     if (body) {
       body.insertAdjacentHTML('beforebegin', `{% set user_country = geoip('user_country')|lower %}{% set locale = user_country ? 'locale-' ~ user_country : 'locale-undecided' %}`);
-
       body.className += '{{ locale }}';
     }
 
-    console.log('The Twig markup has been successfully added to the body section.');
+    if (html) {
+      html.setAttribute('lang', `{{ (geoip('user_country') ?: 'ru')|lower }}`);
+    }
+
+    console.log('The Twig markup has been successfully added to the body section and the html lang attribute has been updated.');
   }
 
-  organizeAssets() {
+  async processCssFiles() {
+    const cssDir = path.join('dist', 'css');
+    const imageExtensions = ['.png', '.jpg', '.jpeg', '.webp', '.gif', '.avif'];
+
+    try {
+      const files = await fs.readdir(cssDir);
+
+      for (const file of files) {
+        const filePath = path.join(cssDir, file);
+        const ext = path.extname(file);
+
+        if (ext === '.css') {
+          let content = await fs.readFile(filePath, 'utf8');
+
+          content = content.replace(/url\((['"]?)([^'")]+)\1\)/g, (match, quote, url) => {
+            const fileName = url.split('/').pop();
+            const fileExt = path.extname(fileName).toLowerCase();
+
+            if (imageExtensions.includes(fileExt)) {
+              return `url(${quote}../images/${fileName}${quote})`;
+            }
+
+            return match;
+          });
+
+          await fs.writeFile(filePath, content, 'utf8');
+
+          console.log(`Processed CSS file: ${file}`);
+        }
+      }
+    } catch (err) {
+      console.log(`CSS directory does not exist: ${cssDir}`);
+    }
+  }
+
+  async organizeAssets() {
     const assetTypes = {
       css: ['css', 'css.map'],
-      images: ['jpg', 'avif', 'webp', 'png', 'svg'],
+      images: ['jpg', 'jpeg', 'avif', 'webp', 'png', 'svg'],
       js: ['js', 'js.map'],
     };
 
-    Object.keys(assetTypes).forEach((type) => {
+    for (const type of Object.keys(assetTypes)) {
       const dirPath = path.join('dist', type);
 
-      if (!fs.existsSync(dirPath)) {
-        fs.mkdirSync(dirPath, { recursive: true });
-      }
-    });
+      await fs.mkdir(dirPath, { recursive: true });
+    }
 
-    fs.readdirSync('dist').forEach((file) => {
+    const files = await fs.readdir('dist');
+
+    for (const file of files) {
       const filePath = path.join('dist', file);
 
-      if (fs.statSync(filePath).isFile()) {
+      if ((await fs.stat(filePath)).isFile()) {
         const ext = file.split('.').pop();
         const fullExt = file.endsWith('.map') ? file.split('.').slice(-2).join('.') : ext;
 
         if (assetTypes.css.includes(fullExt)) {
-          fs.renameSync(filePath, path.join('dist/css', file));
+          await fs.rename(filePath, path.join('dist/css', file));
         } else if (assetTypes.images.includes(ext)) {
-          fs.renameSync(filePath, path.join('dist/images', file));
+          await fs.rename(filePath, path.join('dist/images', file));
         } else if (assetTypes.js.includes(fullExt)) {
-          fs.renameSync(filePath, path.join('dist/js', file));
+          await fs.rename(filePath, path.join('dist/js', file));
         }
       }
-    });
+    }
 
     console.log('Assets were successfully organised and relocated.');
   }
@@ -174,5 +214,5 @@ if (require.main === module) {
   const htmlFilePath = process.argv[2] || 'dist/index.html';
   const processor = new LandsProcessor(htmlFilePath);
 
-  processor.process();
+  processor.process().catch(err => console.error(err));
 }
