1b7dc3b4da6f2f90a0e7677cc99e22155f127a2c
ux-ui-pro <maxim@ux-ui.pro>
2024-11-18 19:48:50 +0300

    release 1.5.0

--- index.js
+++ index.js
@@ -23,9 +23,10 @@ class LandsProcessor {
     await this.processBackgroundImages();
     await this.processHead();
     await this.processGeo();
-    // await this.processGtm();
     await this.organizeAssets();
     await this.processCssFiles();
+    await this.addGtmIncludes();
+    // await this.addSignupFormManager();
     await this.saveHtml();
     await this.organizeMainTwig();
     await this.deleteIndexHtml();
@@ -33,17 +34,14 @@ class LandsProcessor {
 
   async loadHtml() {
     const html = await fs.readFile(this.#htmlFilePath, 'utf8');
-
     this.#dom = new JSDOM(html);
   }
 
   async processLinks() {
     const { document } = this.#dom.window;
-
     document.querySelectorAll('link[href]').forEach((link) => {
       if (link.href.endsWith('.css')) {
         const fileName = link.href.split('/').pop();
-
         link.href = `{{ landing.asset('css/${fileName}') }}`;
       }
     });
@@ -51,77 +49,64 @@ class LandsProcessor {
 
   async processScripts() {
     const { document } = this.#dom.window;
-
     document.querySelectorAll('script[src]').forEach((script) => {
       const fileName = script.src.split('/').pop();
-
       script.src = `{{ landing.asset('js/${fileName}') }}`;
     });
   }
 
   async processImages() {
     const { document } = this.#dom.window;
-
     document.querySelectorAll('img[src]').forEach((img) => {
       const fileName = img.src.split('/').pop();
-
       img.src = `{{ landing.asset('images/${fileName}') }}`;
     });
   }
 
   async processSrcSet() {
     const { document } = this.#dom.window;
-
     document.querySelectorAll('[srcset]').forEach((element) => {
-      const newSrcSet = element.getAttribute('srcset').split(',').map((part) => {
-        const [url, descriptor] = part.trim().split(' ');
-        const fileName = url.split('/').pop();
-
-        return `{{ landing.asset('images/${fileName}') }} ${descriptor}`;
-      }).join(', ');
-
+      const newSrcSet = element
+        .getAttribute('srcset')
+        .split(',')
+        .map((part) => {
+          const [url, descriptor] = part.trim().split(' ');
+          const fileName = url.split('/').pop();
+          return `{{ landing.asset('images/${fileName}') }} ${descriptor}`;
+        })
+        .join(', ');
       element.setAttribute('srcset', newSrcSet);
     });
   }
 
   async processBackgroundImages() {
     const { document } = this.#dom.window;
-
     document.querySelectorAll('[style*="background-image"]').forEach((element) => {
       let styleContent = element.getAttribute('style');
-
       styleContent = styleContent.replace(/url\((['"]?)([^'")]+)\1\)/g, (match, quote, url) => {
         const fileName = url.split('/').pop();
-
         return `url({{ landing.asset('images/${fileName}') }})`;
       });
-
       element.setAttribute('style', styleContent);
     });
   }
 
   async saveHtml() {
     const updatedHtml = this.#dom.serialize();
-
     await fs.writeFile('dist/main.twig', updatedHtml);
-
     console.log('The main.twig file has been successfully created.');
   }
 
   async processHead() {
     const { document } = this.#dom.window;
     const title = document.querySelector('title');
-
     if (title) {
       title.textContent = '{{ landing.yieldArea(\'title\', [\'blocks/title\']) }}';
     }
-
     const head = document.querySelector('head');
-
     if (head) {
       head.insertAdjacentHTML('beforeend', '{{ landing.head() }}');
     }
-
     console.log('The functions have been successfully added to the head section.');
   }
 
@@ -129,34 +114,18 @@ class LandsProcessor {
     const { document } = this.#dom.window;
     const body = document.querySelector('body');
     const html = document.querySelector('html');
-
     if (body) {
-      body.insertAdjacentHTML('beforebegin', `{% set user_country = geoip('user_country')|lower %}{% set locale = user_country ? 'locale-' ~ user_country : 'locale-undecided' %}`);
+      body.insertAdjacentHTML(
+        'beforebegin',
+        `{% set user_country = geoip('user_country')|lower %}{% set locale = user_country ? 'locale-' ~ user_country : 'locale-undecided' %}`
+      );
       body.className += '{{ locale }}';
     }
-
     if (html) {
       html.setAttribute('lang', `{{ (geoip('user_country') ?: 'ru')|lower }}`);
     }
-
     console.log('The Twig markup has been successfully added to the body section and the html lang attribute has been updated.');
   }
-
-  // async processGtm() {
-  //   const { document } = this.#dom.window;
-  //   const head = document.querySelector('head');
-  //   const body = document.querySelector('body');
-  //
-  //   if (head) {
-  //     head.insertAdjacentHTML('beforeend', '{{ getGtmHeadCode() }}');
-  //   }
-  //
-  //   if (body) {
-  //     body.insertAdjacentHTML('afterbegin', '{{ getGtmBodyCode() }}');
-  //   }
-  //
-  //   console.log('The GTM head and body codes have been successfully added.');
-  // }
 
   async processCssFiles() {
     const cssDir = path.join('dist', 'css');
@@ -164,27 +133,20 @@ class LandsProcessor {
 
     try {
       const files = await fs.readdir(cssDir);
-
       for (const file of files) {
         const filePath = path.join(cssDir, file);
         const ext = path.extname(file);
-
         if (ext === '.css') {
           let content = await fs.readFile(filePath, 'utf8');
-
           content = content.replace(/url\((['"]?)([^'")]+)\1\)/g, (match, quote, url) => {
             const fileName = url.split('/').pop();
             const fileExt = path.extname(fileName).toLowerCase();
-
             if (imageExtensions.includes(fileExt)) {
               return `url(${quote}../images/${fileName}${quote})`;
             }
-
             return match;
           });
-
           await fs.writeFile(filePath, content, 'utf8');
-
           console.log(`Processed CSS file: ${file}`);
         }
       }
@@ -199,22 +161,16 @@ class LandsProcessor {
       images: ['jpg', 'jpeg', 'avif', 'webp', 'png', 'svg'],
       js: ['js', 'js.map'],
     };
-
     for (const type of Object.keys(assetTypes)) {
       const dirPath = path.join('dist', type);
-
       await fs.mkdir(dirPath, { recursive: true });
     }
-
     const files = await fs.readdir('dist');
-
     for (const file of files) {
       const filePath = path.join('dist', file);
-
       if ((await fs.stat(filePath)).isFile()) {
         const ext = file.split('.').pop();
         const fullExt = file.endsWith('.map') ? file.split('.').slice(-2).join('.') : ext;
-
         if (assetTypes.css.includes(fullExt)) {
           await fs.rename(filePath, path.join('dist/css', file));
         } else if (assetTypes.images.includes(ext)) {
@@ -224,22 +180,18 @@ class LandsProcessor {
         }
       }
     }
-
-    console.log('Assets were successfully organised and relocated.');
+    console.log('Assets were successfully organized and relocated.');
   }
 
   async organizeMainTwig() {
     const layoutsDir = path.join('dist', 'layouts');
-
     await fs.mkdir(layoutsDir, { recursive: true });
     await fs.rename('dist/main.twig', path.join(layoutsDir, 'main.twig'));
-
     console.log('The main.twig file has been successfully moved to the layouts directory.');
   }
 
   async deleteIndexHtml() {
     const indexPath = path.join('dist', 'index.html');
-
     try {
       await fs.unlink(indexPath);
       console.log('The index.html file has been successfully deleted.');
@@ -247,11 +199,32 @@ class LandsProcessor {
       console.log('The index.html file does not exist or could not be deleted.');
     }
   }
+
+  async addGtmIncludes() {
+    const { document } = this.#dom.window;
+    const head = document.querySelector('head');
+    const body = document.querySelector('body');
+    if (head) {
+      head.insertAdjacentHTML('beforeend', `{% include '../../common/templates/gtm/gtm-head.twig' ignore missing %}`);
+    }
+    if (body) {
+      body.insertAdjacentHTML('afterbegin', `{% include '../../common/templates/gtm/gtm-body.twig' ignore missing %}`);
+    }
+    console.log('GTM includes have been added to head and body.');
+  }
+
+  // async addSignupFormManager() {
+  //   const { document } = this.#dom.window;
+  //   const body = document.querySelector('body');
+  //   if (body) {
+  //     body.insertAdjacentHTML('beforeend', `{% include '../../common/templates/signup/form-manager.twig' ignore missing %}`);
+  //   }
+  //   console.log('Signup form manager include has been added to the body.');
+  // }
 }
 
 if (require.main === module) {
   const htmlFilePath = process.argv[2] || 'dist/index.html';
   const processor = new LandsProcessor(htmlFilePath);
-
-  processor.process().catch(err => console.error(err));
+  processor.process().catch((err) => console.error(err));
 }
